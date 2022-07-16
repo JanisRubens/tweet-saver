f559d578f088d1f7944ffec5bdc1bd2a089b8b18
Janis Rubens <janis.rubens@gmail.com>
2022-07-16 16:07:10 +0300

    Initial project setup with dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex mx-16 mt-16">
+      <div className="border-b w-9/12">
+        <h1 className="font-sans text-xl">Tweet Saver</h1>
+      </div>
     </div>
   );
 }
